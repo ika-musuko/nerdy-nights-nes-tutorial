5d5612587389bca7e8a347d407189511c443630c
sherwyn <sherwyn.sen@gmail.com>
2025-01-07 19:02:52 +0900

    03 - first app compiles

---
+++ main-tutorial/03-first-app/main.asm
@@ -0,0 +1,86 @@
+; information the emulator needs about the game:
+	.inesmap 0 		; mapper 0 = NROM, no bank swapping
+	.inesmir 1		; background mirroring (ignore for now)
+	.inesprg 1		; include 1x 16kb bank of prg code
+	.ineschr 1 		; include 1x 8kb bank of chr data
+
+	.bank	0
+	.org	$8000		; cartridge start
+
+
+; TODO: understand the following
+;	- why txs sets up the stack
+;	- why x is now zero when we increment it
+
+RESET:
+	sei			; disable interrupts (irqs)
+	cld			; disable decimal mode
+
+	ldx	#$40
+	stx	$4017		; disable APU frame irq
+
+	txs			; set up stack
+	inx			; now x = 0
+
+	stx	$2000		; disable NMI
+	stx	$2001		; disable rendering
+	stx	$4010		; disable dmc irq
+
+
+; TODO: understand the following
+;	- what $2002 is
+;	- why we are bit testing it (anding $2002 with A?)
+;	- how that affects the negative flag for the bpl  
+
+; wait for vblank to make sure ppu is ready
+vblankwait1:			
+	bit	$2002		
+	bpl	vblankwait1	
+				
+
+; TODO: understand the following
+;	- why we are doing ", x"
+;	- what locations $0000~$0700 are
+;	- why only $0300 gets value #$fe
+;	- why we are incrementing x
+;	- why we are branching back to clrmem when x is not zero
+clrmem:
+	lda	#$00
+	sta	$0000, x	
+	sta	$0100, x	
+	sta	$0100, x	
+	sta	$0200, x
+	sta	$0400, x
+	sta	$0500, x
+	sta	$0600, x
+	sta	$0700, x
+	lda	#$fe
+	sta	$0300, x
+	inx
+	bne	clrmem
+
+vblankwait2:			; wait for vblank again, PPU is ready after this
+	bit	$2002
+	bpl	vblankwait2
+
+_start:
+	lda	#%1000_0000	; intensify blues on screen
+	sta	$2001
+
+forever:
+	jmp	forever
+
+
+; TODO: understand what an NMI is
+NMI:
+	rti
+
+;;;;;;;;;;;;;;;;;;
+	.bank	1
+	.org	$fffa	; interrupt vector
+	.dw	NMI	; jump to NMI label on nmi interrupt
+	.dw	RESET	; jump to RESET label on reset interrupt
+	.dw	0	; external interrupt unused
+;;;;;;;;;;;;;;;;;;
+
+
